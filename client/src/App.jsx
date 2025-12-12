ea0c4304640002b9b2daaaa317007ce53c999c85
Aditya Raj <adityaraj1389@gmail.com>
2025-12-12 11:32:55 +0530

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     try {
       const res = await api.login(creds);
       setUser(res.data.user);
-    } catch (err) { alert("Login Failed. Try 'admin' / 'pass'"); }
+    } catch (err) { alert("Login Failed. Try 'admin' / 'mypass123'"); }
   };
 
   if (!user) {
@@ -19,7 +19,7 @@ function App() {
         <div className="bg-white p-8 rounded shadow-lg w-96">
           <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
           <input className="border w-full p-2 mb-2" placeholder="Username (admin)" onChange={e => setCreds({...creds, username: e.target.value})} />
-          <input className="border w-full p-2 mb-4" type="password" placeholder="Password (pass)" onChange={e => setCreds({...creds, password: e.target.value})} />
+          <input className="border w-full p-2 mb-4" type="password" placeholder="Password (mypass123)" onChange={e => setCreds({...creds, password: e.target.value})} />
           <button onClick={handleLogin} className="w-full bg-blue-600 text-white py-2 rounded">Login (AWS Lambda)</button>
         </div>
       </div>
